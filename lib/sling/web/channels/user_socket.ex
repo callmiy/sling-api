1efd3b2bf34b35eb20a8c1a05f6a3bc9d8df4edf
maneptha <maneptha@gmail.com>
2017-07-18 12:20:28 +0200

    Updates for heroku.

--- lib/sling/web/channels/user_socket.ex
+++ lib/sling/web/channels/user_socket.ex
@@ -7,8 +7,8 @@ defmodule Sling.Web.UserSocket do
   channel "rooms:*", Sling.Web.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
-  transport :longpoll, Phoenix.Transports.LongPoll
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
+  # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
