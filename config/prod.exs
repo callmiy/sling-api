44580591f7c537de86fe069f422f962ddde3f862
maneptha <maneptha@gmail.com>
2017-07-18 12:34:07 +0200

    Deployment: added check origin to prod.exs end point for my frontend.

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,8 @@ config :sling, Sling.Web.Endpoint,
   ],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  check_origin: ["https://samba6.github.io/sling"] 
 
 config :sling, Sling.Repo,
   adapter: Ecto.Adapters.Postgres,
