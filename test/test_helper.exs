f1201f565e38f783a940afe833b52d742c0ae5ca
maneptha <maneptha@gmail.com>
2017-08-19 13:06:56 +0200

    Test helpers moved into test/support folder: for compilation.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -3,9 +3,3 @@ ExUnit.configure(exclude: [norun: true])
 ExUnit.start()
 
 Ecto.Adapters.SQL.Sandbox.mode(Sling.Repo, :manual)
-
-Code.require_file("messages_helper.exs", __DIR__)
-Code.require_file("users_helper.exs", __DIR__)
-Code.require_file("rooms_helper.exs", __DIR__)
-Code.require_file("user_rooms_helper.exs", __DIR__)
-Code.require_file("conn_helper.exs", __DIR__)
